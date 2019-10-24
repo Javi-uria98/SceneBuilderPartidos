454ae57201ffd5f0121401d5c6c0fd869e07c3c9
Javi-uria98 <55594972+Javi-uria98@users.noreply.github.com>
2019-10-24 19:02:21 +0200

    Segundo commit

--- src/es/javier/Launcher.java
+++ src/es/javier/Launcher.java
@@ -37,8 +37,6 @@ public class Launcher extends Application {
         launch(args);
     }
 
-    //hola
-
     public void cargarFichero() {
         try {
             ObservableList<Partido> listaObservable = Logica.getInstance().getListaPartidos();
